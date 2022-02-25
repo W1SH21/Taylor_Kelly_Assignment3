3b2ebb4ec71f7c89ddd73136b9f201a281415bb0
Benjamin David Taylor <bdt75522@csci-odin.cs.uga.edu>
2022-02-24 23:26:30 -0500

    Fully implemented backend poggers

--- DoublyLinkedList/DoublyLinkedList.h
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -13,7 +13,6 @@ class DoublyLinkedList {
 private:
   NodeType<T> *head;
   NodeType<T> *tail;
-  int size;
   int length;
 
 public:
@@ -21,12 +20,15 @@ public:
   DoublyLinkedList(); //post: list is created
   ~DoublyLinkedList(); //pre: list is created, post: all nodes freed
   
-  void insertItem(T &item);
-  void deleteItem(T &item);
+  void insertItem(T &item); //inserts the value <item> sorted in ascending order
+  void deleteItem(T &item); //searches the list for item containing item and removes it
+  void deleteSubsection(int lbound, int ubound); //deletes items in range lbound <= x <= ubound from the list
   int lengthIs() const; //pre: list exists, post: returns the lenght
   void print(); //prints list to stdout
   void printReverse(); //prints list to stdout in reverse
-  void printElem(); 
+  void printElem();
+  T getMode(); // returns the mode (most common value) in the list
+  void swapAlternate(); //exchanges every other node in the list with it's predacessor.
 }; //DoublyLinkedList<>
   
 #endif
